70d212918f37da8c665fd962df9bedbbee054063
Tony Du <cheeham51@hotmail.com>
2022-04-26 00:04:16 +1000

    Add integration test

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+test("service returns 'Hello there!'", async () => {
+    try {
+        const response = await axios.get(process.env.SERVICE_ENDPOINT, {})
+        expect(response.data).toBe("Hello there!") 
+    } catch (e) {
+        console.error(e)
+        throw e
+    }
+})
