14a1ad63278dd6852274f15e7bf844f256875ee4
Tony Du <cheeham51@hotmail.com>
2022-04-28 23:39:33 +1000

    Add gitignore

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
+// require('dotenv').config()
 
 test("service returns 'Hello there!'", async () => {
     try {
         const response = await axios.get(process.env.SERVICE_ENDPOINT, {})
-        expect(response.data).toBe("Hello there!") 
+        expect(response.data).toBe("Hello there!")
     } catch (e) {
         console.error(e)
         throw e
