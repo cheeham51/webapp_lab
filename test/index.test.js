dd188587025c3f1c2d826d30c8e173226af2947d
Tony Du <cheeham51@hotmail.com>
2022-04-29 17:43:46 +1000

    Correct testing

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ test("service returns 'Hello there!'", async () => {
     try {
         console.log(test)
         const response = await axios.get(process.env.SERVICE_ENDPOINT, {})
-        expect(response.data).toBe("Hello there!--")
+        expect(response.data).toBe("Hello there!")
     } catch (e) {
         console.error(e)
         throw e
