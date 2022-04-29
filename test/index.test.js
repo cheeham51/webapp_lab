7e07a5ce548db63b720381b95b6f58f8e7f4626e
Tony Du <tony@Tonys-MacBook-Pro-2.local>
2022-04-29 14:50:38 +1000

    Add proper testing

--- test/index.test.js
+++ test/index.test.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 test("service returns 'Hello there!'", async () => {
     try {
         console.log(test)
-        // const response = await axios.get(process.env.SERVICE_ENDPOINT, {})
-        // expect(response.data).toBe("Hello there!")
+        const response = await axios.get(process.env.SERVICE_ENDPOINT, {})
+        expect(response.data).toBe("Hello there!")
     } catch (e) {
         console.error(e)
         throw e
