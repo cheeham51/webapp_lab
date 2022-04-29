a65e98ef67ac43229d590b556870dc9227f44d78
Tony Du <cheeham51@hotmail.com>
2022-04-29 20:08:28 +1000

    temporarily break the app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello there!')
+    // res.send('Hello there!')
+    res.sendStatus(500)
 })
 
 const port = 8080;
